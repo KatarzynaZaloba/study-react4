9103c58f7417f49f9f734a7d21baf0f153c30589
Katarzyna Zaloba <kzalobafrontend@gmail.com>
2023-12-07 19:01:35 +0100

    added temporarly commented components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,8 +4,9 @@ export function App() {
     console.log("Działa w konsoli")
     return (
         <>
-            <h1>Nagłówek 1</h1>
-            <h2>Nagłówek 2</h2>
+            {/*<Navigation />*/}
+            {/*<Sidebar />*/}
+            {/*<MainContent />*/}
         </>
     )
 }
